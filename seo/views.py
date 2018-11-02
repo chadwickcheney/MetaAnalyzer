c3a0aed771024f888d36c5380a91a606023d0ab5
Chadwick Cheney <chadwickcheney@outlook.com>
2018-11-02 14:27:33 -0500

    Fixed form method.POST

--- seo/views.py
+++ seo/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import time
+import requests
+from bs4 import BeautifulSoup
+from .forms import UrlForm
+
+class Spider:
+    def __init__(self,request):
+        self.form = UrlForm(request.POST)
+        self.table = self.get_table_data()
+        self.context = {'form':self.form,'table':self.table}
+
+    def get_table_data(self):
+        #This will the dictionary contatining all the data scraped from the html document
+        dictionary={'title':'This is a title','description':'This is a description'}
+        return dictionary
 
 def index(request):
-    return render(request,'main.html')
+    spider = Spider(request)
+    return render(request,'main.html',spider.context)
